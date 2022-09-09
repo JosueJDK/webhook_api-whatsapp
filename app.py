618706ef1919f0c3bd97cc816d04e874171a5e60
JouCode <salazarjosue2604@gmail.com>
2022-09-09 15:03:57 -0500

    Ver. 01

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+app = Flask(__name__)
+ 
+
+@app.route('/', methods=['GET'])
+def home():
+   return "Home!", 200
+
+@app.route('/webhooks', methods=['POST','GET'])
+def webhook():
+   if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+       if not request.args.get("hub.verify_token")== "YOUR VARIFICATION TOKEN":
+           return "Verification token missmatch", 403
+       return request.args['hub.challenge'], 200
+   return "Hello world", 200
+ 
+if __name__ == "__main__":
+   app.run(debug=True)
